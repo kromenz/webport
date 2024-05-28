6c48f3c76429326d2b1de75e982979fc7519395c
kromenz <rafael.pires.andre@ipvc.pt>
2024-05-28 13:56:44 +0100

    cv to new tab finished

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import "./navbar.css";
 import MobileNav from "./MobileNav/MobileNav";
+import PDF from "../../CV.pdf";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
+  };
+
+  const openPDF = () => {
+    window.open(PDF, "_blank");
   };
 
   return (
@@ -28,8 +33,9 @@ const Navbar = () => {
             <li>
               <a className="menu-item">Contact Me</a>
             </li>
-            <button className="contact-btn" onClick={() => {}}>
-              Hire Me
+
+            <button className="contact-btn" onClick={openPDF}>
+              My CV
             </button>
           </ul>
           <button className="menu-btn" onClick={toggleMenu}>
