0c9b00d02962c8abe1afa61fb20bba382c627ad3
kromenz <rafael.pires.andre@ipvc.pt>
2024-06-19 01:08:22 +0100

    icon bug

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -10,17 +10,17 @@ const ContactMe = () => {
       <div className="contact-content">
         <div style={{ flex: 1 }}>
           <ContactInfoCard
-            iconUrl="./assets/images/email.png"
+            iconUrl="/assets/images/email.png"
             text="andrerafael892@gmail.com"
             href="mailto:andrerafael892@gmail.com"
           />
           <ContactInfoCard
-            iconUrl="./assets/images/github.svg"
+            iconUrl="/assets/images/github.png"
             text="github.com/kromenz"
             href="https://github.com/kromenz"
           />
           <ContactInfoCard
-            iconUrl="./assets/images/linkdin.png"
+            iconUrl="/assets/images/linkdin.png"
             text="linkdin"
             href="https://linkedin.com/in/rafael-andré/"
           />
